b53a7ea949b175d315ae04872ed61e7d05878e88
IMenezes <107199192+IMenezes-GH@users.noreply.github.com>
2023-09-02 21:05:30 -0300

    Merge da implementação do Matzh

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 const { Client, GatewayIntentBits } = require("discord.js");
-const Conta = require('./Conta')
-require('dotenv').config();
-const token = process.env.token
+const Conta = require('./contas.js');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+require('dotenv').config();
+
+const token = process.env.token
+const prefix = '+'
+
+const client = new Client({
+     intents: [
+        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.MessageContent
+    ] 
+});
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -13,6 +22,42 @@ client.on('messageCreate', (message) => {
     const conteudo = message.content.split(' ').length === 1 ? message.content : message.content.split(' ');
     const autor = message.author;
 
+    // Verifique se a mensagem começa com o prefixo e não foi enviada pelo bot
+    if (!conteudo.startsWith(prefix) || autor.bot) return;
+
+    // Divide a mensagem em partes
+    const args = conteudo.slice(prefix.length).trim().split(/ +/);
+    const comando = args.shift().toLowerCase();
+
+    if (!Conta.db.has(autor.id)){
+        Conta.db.set(autor.id, new Conta({id: autor.id, nome: autor.globalName, username: autor.username, saldo: 0, ultimoTrabalho: 0}))
+    } 
+    
+    const conta = Conta.db.get(autor.id)
+
+    if(comando === "conta"){
+        const dados = conta.mostrarConta()
+        message.reply(`Usuário ${dados.nome} possui ${dados.saldo} dinheiros.`)
+    }
+
+    if(comando === "trab"){
+        const trabalho = conta.trabalhar()
+        if (trabalho){
+            message.reply(`Usuário ${conta.nome} trabalhou, ganhando ${trabalho} dinheiros`)
+        } else {
+            message.reply(`Usuário ${conta.nome} não pode trabalhar!`)
+        }
+    }
+
+    if (comando === "con") {
+        const mensagem = Conta.db;
+        mensagem.forEach((conta) => {
+            console.log(conta);
+            // Verifique se a mensagem foi enviada pelo bot
+            if (!message.author.bot) {
+                message.channel.send(`${conta.id}\n${conta.nome}\n${conta.saldo}`);
+            }
+        })
 
     if (autor.bot) return
 
@@ -69,6 +114,7 @@ client.on('messageCreate', (message) => {
 
         })
     }
-});
+    }
+})
 
 client.login(token);
