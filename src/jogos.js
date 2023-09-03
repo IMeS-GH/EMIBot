3db4872b2ed10f7e3916fba3fd665fc61acda8fa
IMenezes <107199192+IMenezes-GH@users.noreply.github.com>
2023-09-03 18:54:58 -0300

    desfaz alterações do merge

--- src/jogos.js
+++ src/jogos.js
@@ -50,39 +50,5 @@ class Jokenpo extends Jogo{
 
 }
 
-class ApostaCores extends Jogo{
-    
-    static coresAposta = ['vermelho', 'preto', 'branco']
-    
-    constructor(autor, corAposta, valorAposta){
-       super(autor)
 
-       valorAposta = Number(valorAposta)
-       contaAutor = Conta.db.get(autor.username)
-
-       if (!coresAposta.includes(corAposta) || isNaN(valorAposta)) return {status: 'fail', message: 'Uso correto: !aposta <vermelho/preto/branco> <valor>'}
-       if (valorAposta > contaAutor.saldo) return {status: 'fail', message: 'Você não tem saldo suficiente para fazer essa aposta.'}
-
-        resultado = Math.floor(Math.random() * 100) + 1;
-        mensagemResultado = `Você apostou ${valorAposta} na cor ${corAposta}.\n`;
-
-        if (corAposta === 'branco' && resultado <= 10) { // 10% de chance para branco
-            const ganho = valorAposta * 9; // Pagamento 9x
-            conta.saldo += ganho;
-            mensagemResultado += `Você ganhou! Seu saldo agora é ${conta.saldo}.`;
-        } else if (
-            (corAposta === 'vermelho' && resultado <= 45) ||
-            (corAposta === 'preto' && resultado <= 45)
-        ) {
-            const ganho = valorAposta * 2; // Pagamento 2x
-            conta.saldo += ganho;
-            mensagemResultado += `Você ganhou! Seu saldo agora é ${conta.saldo}.`;
-        } else {
-            conta.saldo -= valorAposta;
-            mensagemResultado += `Você perdeu! Seu saldo agora é ${conta.saldo}.`;
-        }
-    }
-}
-
-
-module.exports = {Jokenpo, ApostaCores}
+module.exports = {Jokenpo}
