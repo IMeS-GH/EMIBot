020686c6ad2fe711b1f45ee2dd4815c9404e733e
Altovisk <ericaltao13@gmail.com>
2023-09-04 15:48:49 -0300

    db JSON cadastrando

--- src/contas.js
+++ src/contas.js
@@ -1,22 +1,29 @@
-class Conta{
-    static db = new Map()
+const fs = require('fs');
+const path = require('path');
 
-    constructor({id, nome, username}){
+class Conta {
+    static db = new Map()
+    static filePath = path.join(__dirname, 'database', 'users.json');
+
+    constructor({ id, nome, username }) {
         this.id = id;
         this.nome = nome;
         this.username = username;
         this.saldo = 0;
         this.ultimoTrabalho = 0;
-    }
 
-    mostrarConta(){
+        const conta = this;
+        
+        this.cadastrarUser(conta)
+    }
+    mostrarConta() {
         return this;
     }
 
-    trabalhar(){
+    trabalhar() {
         const horaTrabalho = new Date().valueOf()
-        if (horaTrabalho - this.ultimoTrabalho > 30000){
-            
+        if (horaTrabalho - this.ultimoTrabalho > 30000) {
+
             const dinheiro = 100 + Math.ceil(Math.random() * 500);
             this.ultimoTrabalho = horaTrabalho;
             this.saldo += dinheiro;
@@ -39,6 +46,24 @@ class Conta{
         destino.saldo += valor;
         return `Transferência bem-sucedida! ${this.nome} transferiu ${valor} dinheiros para ${destino.nome}.`;
     }
-}
+    cadastrarUser(conta) {
+       const dados = this.carregarDados();
 
+         dados.push({
+            id: conta.id,
+            nome: conta.nome,
+            username: conta.username,
+            saldo: conta.saldo,
+            ultimoTrabalho: conta.ultimoTrabalho
+        });
+        
+        this.salvarDados(dados);
+    }
+    carregarDados() {
+        return JSON.parse(fs.readFileSync(Conta.filePath, "utf-8"));
+    }
+    salvarDados(dados){
+       return fs.writeFileSync(Conta.filePath, JSON.stringify(dados, null, " "));
+    }
+}
 module.exports = Conta;
--- src/index.js
+++ src/index.js
@@ -23,26 +23,22 @@ client.on('messageCreate', (message) => {
         if (!Conta.db.has(autor.username)) {
             Conta.db.set(autor.username, new Conta({ id: autor.id, nome: autor.globalName, username: autor.username }))
             console.log(`${autor.globalName} cadastrado`);
-            const conta = Conta.db.get(autor.username)
-            console.log(conta)
         }
 
         const args = conteudo.slice(prefix.length).split(' ');
 
         if (conteudo[0].startsWith(prefix)) { // Verifique se a mensagem começa com o prefixo
             const comando = args.shift().toLowerCase(); // Divide a mensagem em partes
+            const conta = Conta.db.get(autor.username) // Conta do usuário do contexto da mensagem
 
             if (comando === "conta") {
-                const conta = Conta.db.get(autor.username) // Conta do usuário do contexto da mensagem
                 const dados = conta.mostrarConta()
                 message.reply(`> **ID:** ${dados.id}\n> **Usuário:**${dados.username}\n> **Nome:**${dados.nome}\n> **Saldo:** ${conta.saldo}\n`)
             };
 
             if (comando === "trab" || comando === "trabalhar") {
-                const conta = Conta.db.get(autor.username) // Conta do usuário do contexto da mensagem
-                console.log(conta.saldo, conta.nome)
                 const salario = conta.trabalhar()
-                console.log(conta.saldo, conta.nome)
+
                 if (salario) {
                     message.reply(`Usuário ${conta.nome} trabalhou, ganhando ${salario} dinheiros.\n\nSaldo atual: ${conta.saldo}`)
                 } else {
@@ -51,19 +47,14 @@ client.on('messageCreate', (message) => {
             };
 
             if (comando === "contas") {
-                const conta = Conta.db.get(autor.username) // Conta do usuário do contexto da mensagem
                 const mensagem = Conta.db;
                 mensagem.forEach((conta) => {
                     console.log(conta);
-                    // Verifique se a mensagem foi enviada pelo bot
-                    if (!message.author.bot) {
                         message.channel.send(`> **ID:** ${conta.id}\n> **Usuário:**${conta.username}\n> **Nome:**${conta.nome}\n> **Saldo:** ${conta.saldo}\n`);
-                    }
                 })
             }
 
             if (comando === "trans" || comando === "transferir") {
-                const conta = Conta.db.get(autor.username) // Conta do usuário do contexto da mensagem
                 if (args.length === 2) {
                     const contaDestinatario = Conta.db.get(args[0])
                     const valorTransferencia = Number(args[1])
@@ -78,7 +69,6 @@ client.on('messageCreate', (message) => {
             }
 
             if (comando === "clonar") {
-                const conta = Conta.db.get(autor.username) // Conta do usuário do contexto da mensagem
                 if (Conta.db.has('laranja')) {
                     message.reply("Já existe uma conta laranja no banco de dados.")
                     return
